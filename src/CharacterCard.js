c73618dc8bc878203ec39dbe32b5141ec5271871
mrAmjadChel <amjadchelaeh.8@gmail.com>
2022-08-10 21:12:51 +0700

    update

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -5,8 +5,12 @@ export default function CharacterCard(props) {
 
     const [active, setActive] = useState(false);
     const activate = () => {
-        setActive(true)
+        if(!active){
+            setActive(true)
+            props.activationHandler(props.value)
+        }
     }
+       
 
     const className = `card ${active ? 'activeCard': ''}`
     
