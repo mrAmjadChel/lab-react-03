de61abd59144b7dbe013e9dbe957832c81d0231d
mrAmjadChel <amjadchelaeh.8@gmail.com>
2022-08-10 20:14:33 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 
-const word = "Hello";
 function App() {
   return (
     <div>
-      {
-        Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
-      }
+      <WordCard value="hello"/>
     </div>
   );
 }
