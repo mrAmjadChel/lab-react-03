b6255194b7ddad46074391b7d2c082070f896b32
mrAmjadChel <amjadchelaeh.8@gmail.com>
2022-08-10 19:55:01 +0700

    often

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
+import CharacterCard from './CharacterCard';
 
 function App() {
 
   return (
     <div>
-      Hello World
+      <CharacterCard value="h"/>
+      <CharacterCard value="i"/>
     </div>
   );
 
